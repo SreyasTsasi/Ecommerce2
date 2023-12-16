82ca0b61d454357ec246588af7defb7355b46993
SreyasTsasi <sreyuzzzksd@gmail.com>
2023-12-16 11:59:47 +0530

    Ecommerce

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -1,4 +1,4 @@
-// server.js
+
 
 const express = require('express');
 const cors = require('cors');
@@ -6,11 +6,11 @@ const app = express();
 
 app.use(cors());
 
-const productRouter = require('./router'); // Import the router
+const productRouter = require('./router'); 
 
-app.use('/api', productRouter); // Use the router with '/api' as a base path
+app.use('/api', productRouter); 
 
-// Example product data (Replace this with your actual product data)
+
 const products = [
     {
     id: 1,
@@ -24,14 +24,14 @@ const products = [
       image: "./src/images/Ecommerc.jpg",
     price: 29.99,
   },
-  // Add more products as needed
+
 ];
 
 app.get('/products', (req, res) => {
   res.json(products);
 });
 
-const PORT = 5000; // Choose any port you prefer
+const PORT = 5000; 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
